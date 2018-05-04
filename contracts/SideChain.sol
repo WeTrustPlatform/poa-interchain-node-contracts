98165b3f7437092450a966a0927d6b0d283c8296
shine2lay <aungshine@gmail.com>
2018-05-04 12:56:00 -0700

    check-in MainChain, MultiSigOwnable and Freezable Tests done

--- contracts/SideChain.sol
+++ contracts/SideChain.sol
@@ -1,80 +1,258 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.21;
 
-contract SideChain {
+import './libs/MultiSigOwnable.sol';
+import './libs/Freezable.sol';
 
-	event hashedMessage(address to, bytes32 hashed, bytes32 msgHash);
+contract SideChain is Freezable {
+	/*
+ 	 *  Events
+ 	 */
+	event Confirmation(address indexed sender, bytes32 indexed txHash);
+	event Revocation(address indexed sender, bytes32 indexed txHash);
+	event Submission(bytes32 indexed txHash);
+	event Execution(bytes32 indexed txHash);
+	event ExecutionFailure(bytes32 indexed txHash);
+	event Deposit(address indexed sender, address indexed to, uint value);
 	event signatureAdded(bytes32 txHash, uint8 v, bytes32 r, bytes32 s);
 
-	modifier onlyByAuthority (address toCheck) {
-		require(isAuthority[toCheck]);
+
+	modifier notNull(address _address) {
+		require(_address != 0);
 		_;
 	}
 
-	address[] public authorities;
-	mapping (address => bool) isAuthority;
-	uint256 public required;
-	mapping (bytes32 => Transaction) transactions;
-	mapping (bytes32 => mapping(address => bool)) signedTx;
+	mapping (bytes32 => Transaction) sideChainTx;
+	mapping (bytes32 => mapping(address => bool)) isSignedSC;
+	uint256 sideChainTxCount;
+
+	mapping (bytes32 => Transaction) mainChainTx;
+	mapping (bytes32 => Signatures) mainChainSigs;
+	mapping (bytes32 => mapping(address => bool)) isSignedMC;
+	uint256 mainChainTxCount;
+
 
 	struct Transaction {
-		address to;
-		uint256 amount;
+		address destination;
+		uint256 value;
+		bytes data;
 		bool executed;
-		bool exists;
-		signatures confirmations;
 	}
 
-	struct signatures {
+	struct Signatures {
 		uint8[] v;
 		bytes32[] r;
 		bytes32[] s;
 	}
 
-	function SideChain(address[] _authorities, uint256 _required) public{
-		authorities = _authorities;
-		required = _required;
+	function SideChain(address[] _owners, uint8 _required) Freezable(_owners, _required) public {
+	}
 
-		for (uint i=0; i< _authorities.length; i++) {
-			require(!isAuthority[_authorities[i]] && _authorities[i] != 0);
-			isAuthority[_authorities[i]] = true;
+	function deposit(address to) notNull(to) payable public {
+		require(msg.value > 0);
+		emit Deposit(msg.sender, to, msg.value);
+	}
+
+	// note we don't need v,r,s (signature because we don't need to store it) we'll be checking if msg.sender is an owner
+	function submitTransactionSC(bytes32 txHash, address destination, uint256 value, bytes data) public {
+		require(!isSignedSC[txHash][msg.sender]);
+
+		if(sideChainTx[txHash].destination != address(0)){
+			require(sideChainTx[txHash].destination == destination);
+			require(sideChainTx[txHash].value == value);
+		} else {
+			addTransactionSC(txHash, destination, value, data);
+		}
+
+		isSignedSC[txHash][msg.sender] = true;
+	}
+
+	/// @dev Allows an owner to confirm a transaction.
+	function confirmTransaction(bytes32 txHash)
+	public
+	ownerExists(msg.sender)
+	{
+		require(!isSignedSC[txHash][msg.sender]);
+		require(sideChainTx[txHash].destination != 0);
+		isSignedSC[txHash][msg.sender] = true;
+		emit Confirmation(msg.sender, txHash);
+		executeTransaction(txHash);
+	}
+
+	/// @dev Allows an owner to revoke a confirmation for a transaction.
+	function revokeConfirmation(bytes32 txHash)
+	public
+	ownerExists(msg.sender)
+	{
+		require(!sideChainTx[txHash].executed);
+		require(isSignedSC[txHash][msg.sender]);
+
+		isSignedSC[txHash][msg.sender] = false;
+		emit Revocation(msg.sender, txHash);
+	}
+
+	/// @dev Allows anyone to execute a confirmed transaction.
+	function executeTransaction(bytes32 txHash)
+	public
+	ownerExists(msg.sender)
+	{
+		require(isSignedSC[txHash][msg.sender]);
+		require(!sideChainTx[txHash].executed);
+		if (isConfirmed(txHash)) {
+			Transaction storage txn = sideChainTx[txHash];
+			txn.executed = true;
+			if (external_call(txn.destination, txn.value, txn.data.length, txn.data))
+				emit Execution(txHash);
+			else {
+				emit ExecutionFailure(txHash);
+				txn.executed = false;
+			}
 		}
 	}
 
-
-	function submitSignature(bytes32 txHash, address to, uint256 amount, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public
-		onlyByAuthority(msg.sender)
+	/// @dev Returns the confirmation status of a transaction.
+	/// @return Confirmation status.
+	function isConfirmed(bytes32 txHash)
+	public
+	constant
+	returns (bool)
 	{
-		require(!signedTx[txHash][msg.sender]);
+		uint count = 0;
+		for (uint i=0; i<owners.length; i++) {
+			if (isSignedSC[txHash][owners[i]])
+				count += 1;
+			if (count == required)
+				return true;
+		}
+	}
 
-		bytes32 hashed = keccak256(txHash, to, amount);
+	/*
+ * Web3 call functions
+ */
+	/// @dev Returns number of isSignedSC of a transaction.
+	/// @return Number of isSignedSC.
+	function getConfirmationCount(bytes32 txHash)
+	public
+	constant
+	returns (uint count)
+	{
+		for (uint i=0; i<owners.length; i++)
+			if (isSignedSC[txHash][owners[i]])
+				count += 1;
+	}
 
-		emit hashedMessage(to, hashed, msgHash);
-		require(hashed == msgHash);
+	/// @dev Returns list of owners.
+	/// @return List of owner addresses.
+	function getOwners()
+	public
+	constant
+	returns (address[])
+	{
+		return owners;
+	}
 
-		if(transactions[txHash].exists){
-			require(transactions[txHash].to == to);
-			require(transactions[txHash].amount == amount);
+	// @dev Returns array with owner addresses, which confirmed transaction.
+	/// @return Returns array of owner addresses.
+	function getSigners(bytes32 txHash)
+	public
+	constant
+	returns (address[] _isSignedSC)
+	{
+		address[] memory isSignedSCTemp = new address[](owners.length);
+		uint count = 0;
+		uint i;
+		for (i=0; i<owners.length; i++)
+			if (isSignedSC[txHash][owners[i]]) {
+				isSignedSCTemp[count] = owners[i];
+				count += 1;
+			}
+		_isSignedSC = new address[](count);
+		for (i=0; i<count; i++)
+			_isSignedSC[i] = isSignedSCTemp[i];
+	}
 
 
+	function addTransactionSC(bytes32 txHash, address destination, uint value, bytes data)
+	internal
+	notNull(destination)
+	{
+		sideChainTx[txHash] = Transaction({
+			destination: destination,
+			value: value,
+		  executed: false,
+			data: data
+			});
+		sideChainTxCount += 1;
+	}
+
+	// call has been separated into its own function in order to take advantage
+	// of the Solidity's code generator to produce a loop that copies tx.data into memory.
+	function external_call(address destination, uint value, uint dataLength, bytes data) private returns (bool) {
+		bool result;
+		assembly {
+			let x := mload(0x40)   // "Allocate" memory for output (0x40 is where "free memory" pointer is stored by convention)
+			let d := add(data, 32) // First 32 bytes are the padded length of data, so exclude that
+			result := call(
+			sub(gas, 34710),   // 34710 is the value that solidity is currently emitting
+			// It includes callGas (700) + callVeryLow (3, to pay for SUB) + callValueTransferGas (9000) +
+			// callNewAccountGas (25000, in case the destination address does not exist and needs creating)
+			destination,
+			value,
+			d,
+			dataLength,        // Size of the input (in bytes) - this is what fixes the padding problem
+			x,
+			0                  // Output is ignored, therefore the output size is zero
+			)
+		}
+		return result;
+	}
+
+	//////////////////////////////////////////////
+	// SUBMIT SIGNATURES TO WITHDRAW ON MAINCHAIN
+	//////////////////////////////////////////////
+
+	function submitSignatureMC(bytes32 msgHash, bytes32 txHash, address destination, uint256 value, bytes data, uint8 v, bytes32 r, bytes32 s) public
+		ownerExists(msg.sender)
+		notNull(destination)
+	{
+		require(!isSignedMC[txHash][msg.sender]);
+
+		bytes32 hashedTxParams = keccak256(txHash, destination, value, data);
+		require(hashedTxParams == msgHash);
+
+		if(mainChainTx[txHash].destination != address(0)){
+			require(mainChainTx[txHash].destination == destination);
+			require(mainChainTx[txHash].value == value);
 		} else {
-			transactions[txHash].to = to;
-			transactions[txHash].amount = amount;
-			transactions[txHash].executed = false;
-			transactions[txHash].exists = true;
-
+			addTransactionMC(txHash, destination, value, data);
 		}
 
-		transactions[txHash].confirmations.v.push(v);
-		transactions[txHash].confirmations.r.push(r);
-		transactions[txHash].confirmations.s.push(s);
+		addSignatureMC(txHash, v, r, s);
 
-		signedTx[txHash][msg.sender] = true;
+		isSignedMC[txHash][msg.sender] = true;
 		emit signatureAdded(txHash, v, r, s);
 	}
 
-	function getTransaction(bytes32 txHash) public returns (uint8[] v, bytes32[] r, bytes32[] s) {
-		require(transactions[txHash].exists);
-		return (transactions[txHash].confirmations.v, transactions[txHash].confirmations.r, transactions[txHash].confirmations.s);
+	function addSignatureMC(bytes32 txHash, uint8 v, bytes32 r, bytes32 s) internal {
+		mainChainSigs[txHash].v.push(v);
+		mainChainSigs[txHash].r.push(r);
+		mainChainSigs[txHash].s.push(s);
 	}
 
+	function addTransactionMC(bytes32 txHash, address destination, uint256 value, bytes data)
+	internal
+	notNull(destination)
+	{
+		mainChainTx[txHash] = Transaction({
+			destination: destination,
+			value: value,
+			executed: false,
+			data: data
+			});
+		mainChainTxCount += 1;
+	}
+
+	function getTransactionMC(bytes32 txHash) public view returns (uint8[] v, bytes32[] r, bytes32[] s) {
+		require(mainChainTx[txHash].destination != address(0));
+		return (mainChainSigs[txHash].v, mainChainSigs[txHash].r, mainChainSigs[txHash].s);
+	}
 }
--- test/MultiSigOwnableUnitTest/addOwnerUnitTest.js
+++ test/MultiSigOwnableUnitTest/addOwnerUnitTest.js
@@ -11,20 +11,24 @@ contract('MultiSigOwnable: AddOwner Unit Test', function(accounts) {
   });
 
   it("checks that addOwner work as intended if all the condition are valid", async function () {
+    const newOwner = accounts[5];
+
     const ownersList = await ownableInstance.getOwners.call();
 
-    assert.notOk(ownersList.includes(accounts[5])); // first check that address to be added doesn't exist
+    assert.notOk(ownersList.includes(newOwner)); // first check that address to be added doesn't exist
 
     const addOwnerEncodedDataField = ownableInstance.contract.addOwner.getData(accounts[5]);
     await ownableInstance.callSelf(addOwnerEncodedDataField);
     const newOwnersList = await ownableInstance.getOwners.call();
 
     assert.equal(newOwnersList.length, 4);
-    assert.ok(newOwnersList.includes(accounts[5]));
+    assert.ok(newOwnersList.includes(newOwner));
   });
 
   it("revert if not called from the smart contract Address", async function () {
-    const addOwnerEncodedDataField = ownableInstance.contract.addOwner.getData(accounts[5]);
+    const newOwner = accounts[5];
+
+    const addOwnerEncodedDataField = ownableInstance.contract.addOwner.getData(newOwner);
 
     // await utils.assertRevert(ownableInstance.sendTransaction({from: accounts[0], data: addOwnerEncodedDataField}));
     // check that calling the correct doesn't revert
@@ -32,21 +36,24 @@ contract('MultiSigOwnable: AddOwner Unit Test', function(accounts) {
   });
 
   it("revert if owner already exists", async function () {
+    const newOwner = accounts[5];
 
     const ownersList = await ownableInstance.getOwners.call();
 
-    assert.notOk(ownersList.includes(accounts[5])); // first check that address to be added doesn't exist
+    assert.notOk(ownersList.includes(newOwner)); // first check that address to be added doesn't exist
 
     const addOwnerEncodedDataField = ownableInstance.contract.addOwner.getData(ownersList[0]);
     await ownableInstance.callSelf(addOwnerEncodedDataField);
     const newOwnersList = await ownableInstance.getOwners.call();
 
     assert.equal(newOwnersList.length, 3);
-    assert.notOk(newOwnersList.includes(accounts[5]))
+    assert.notOk(newOwnersList.includes(newOwner))
   });
 
   it("checks that ownerAddition event is emitted properly", async function () {
-    const addOwnerEncodedDataField = ownableInstance.contract.addOwner.getData(accounts[5]);
+    const newOwner = accounts[5];
+
+    const addOwnerEncodedDataField = ownableInstance.contract.addOwner.getData(newOwner);
     const res = await ownableInstance.callSelf(addOwnerEncodedDataField);
     const log = res.logs[0];
     assert.equal(log.event, 'OwnerAddition');
