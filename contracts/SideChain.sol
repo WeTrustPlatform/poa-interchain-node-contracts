855b820d172296cad5e335ed06b4ea2859169276
shine2lay <aungshine@gmail.com>
2018-05-11 14:21:38 -0700

    add documentation + clean up sidechain.sol + minor fixes to mainchain.sol

--- contracts/SideChain.sol
+++ contracts/SideChain.sol
@@ -4,15 +4,14 @@ import './libs/MultiSigOwnable.sol';
 import './libs/Freezable.sol';
 
 contract SideChain is Freezable {
-
-	/*
-	 * Version information
-	 */
+	//////////////////////
+	//	 CONSTANTS
+	/////////////////////
 	uint8 public constant VERSION = 1;
 
-	/*
- 	 *  Events
- 	 */
+	//////////////////////
+	//	 EVENTS
+	/////////////////////
 	event Confirmation(address indexed sender, bytes32 indexed txHash);
 	event Revocation(address indexed sender, bytes32 indexed txHash);
 	event Submission(bytes32 indexed txHash);
@@ -21,12 +20,17 @@ contract SideChain is Freezable {
 	event Deposit(address indexed sender, address indexed to, uint value);
 	event SignatureAdded(bytes32 txHash, uint8 v, bytes32 r, bytes32 s);
 
-
+	//////////////////////
+	//	 Modifiers
+	/////////////////////
 	modifier notNull(address _address) {
 		require(_address != 0);
 		_;
 	}
 
+	////////////////////////
+	//	 Storage Variables
+	////////////////////////
 	mapping (bytes32 => Transaction) sideChainTx;
 	mapping (bytes32 => mapping(address => bool)) isSignedSC;
 	uint256 sideChainTxCount;
@@ -36,7 +40,9 @@ contract SideChain is Freezable {
 	mapping (bytes32 => mapping(address => bool)) public isSignedMC;
 	uint256 mainChainTxCount;
 
-
+	////////////////////////
+	//	 Structs
+	////////////////////////
 	struct Transaction {
 		address destination;
 		uint256 value;
@@ -50,15 +56,31 @@ contract SideChain is Freezable {
 		bytes32[] s;
 	}
 
+	/////////////////////////
+	//	 Public Functions
+	////////////////////////
+
+	/// @dev Contract constructor sets initial owners and required number of confirmations.
+	/// @param _owners List of initial owners.
+	/// @param _required Number of required confirmations.
 	function SideChain(address[] _owners, uint8 _required) Freezable(_owners, _required) public {
 	}
 
-	function deposit(address to) notNull(to) payable public {
+	/// @dev Allows to deposit ETH into the contract
+	/// @param to the destination address where user wish to received the funds on mainchain
+	function deposit(address to)
+	  notNull(to)
+	  payable
+	  public {
 		require(msg.value > 0);
 		emit Deposit(msg.sender, to, msg.value);
 	}
 
-	// note we don't need v,r,s (signature because we don't need to store it) we'll be checking if msg.sender is an owner
+	/// @dev submit transaction to be processed pending the approvals
+	/// @param txHash transaction hash of the deposit tx in main chain
+	/// @param destination destination provided in deposit tx in main chain
+	/// @param value msg.value of deposit tx in main chain
+	/// @param data data of deposit tx in main chain
 	function submitTransactionSC(bytes32 txHash, address destination, uint256 value, bytes data) public {
 		require(!isSignedSC[txHash][msg.sender]);
 
@@ -72,23 +94,11 @@ contract SideChain is Freezable {
 		isSignedSC[txHash][msg.sender] = true;
 	}
 
-	/// @dev Allows an owner to confirm a transaction.
-	function confirmTransaction(bytes32 txHash)
-	internal
-	ownerExists(msg.sender)
-	{
-		require(!isSignedSC[txHash][msg.sender]);
-		require(sideChainTx[txHash].destination != 0);
-		isSignedSC[txHash][msg.sender] = true;
-		emit Confirmation(msg.sender, txHash);
-		executeTransaction(txHash);
-	}
-
 	/// @dev Allows an owner to revoke a confirmation for a transaction.
+	/// @param txHash to revoke confirmation
 	function revokeConfirmation(bytes32 txHash)
-	public
-	ownerExists(msg.sender)
-	{
+	  ownerExists(msg.sender)
+	  public {
 		require(!sideChainTx[txHash].executed);
 		require(isSignedSC[txHash][msg.sender]);
 
@@ -97,10 +107,10 @@ contract SideChain is Freezable {
 	}
 
 	/// @dev Allows anyone to execute a confirmed transaction.
+	/// @param txHash to be executed
 	function executeTransaction(bytes32 txHash)
-	public
-	ownerExists(msg.sender)
-	{
+	  ownerExists(msg.sender)
+	  public {
 		require(isSignedSC[txHash][msg.sender]);
 		require(!sideChainTx[txHash].executed);
 		if (isConfirmed(txHash)) {
@@ -118,10 +128,9 @@ contract SideChain is Freezable {
 	/// @dev Returns the confirmation status of a transaction.
 	/// @return Confirmation status.
 	function isConfirmed(bytes32 txHash)
-	public
-	constant
-	returns (bool)
-	{
+	  view
+	  public
+	  returns(bool) {
 		uint count = 0;
 		for (uint i=0; i<owners.length; i++) {
 			if (isSignedSC[txHash][owners[i]])
@@ -131,28 +140,23 @@ contract SideChain is Freezable {
 		}
 	}
 
-	/*
- * Web3 call functions
- */
-	/// @dev Returns number of isSignedSC of a transaction.
-	/// @return Number of isSignedSC.
+	/// @dev Returns number of confirmations of a transaction.
+	/// @return Number of confirmations.
 	function getConfirmationCount(bytes32 txHash)
-	public
-	constant
-	returns (uint count)
-	{
+	  view
+	  public
+	  returns(uint count) {
 		for (uint i=0; i<owners.length; i++)
 			if (isSignedSC[txHash][owners[i]])
 				count += 1;
 	}
 
-	// @dev Returns array with owner addresses, which confirmed transaction.
+	/// @dev Returns array with owner addresses, which confirmed transaction.
 	/// @return Returns array of owner addresses.
 	function getSigners(bytes32 txHash)
-	public
-	constant
-	returns (address[] _isSignedSC)
-	{
+	  view
+	  public
+	  returns(address[] _isSignedSC) {
 		address[] memory isSignedSCTemp = new address[](owners.length);
 		uint count = 0;
 		uint i;
@@ -166,11 +170,19 @@ contract SideChain is Freezable {
 			_isSignedSC[i] = isSignedSCTemp[i];
 	}
 
+	/////////////////////////
+	//	 Private Functions
+	////////////////////////
 
+
+	/// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
+	/// @param txHash Transaction transactionHash
+	/// @param destination Transaction target address.
+	/// @param value Transaction ether value.
+	/// @param data Transaction data payload.
 	function addTransactionSC(bytes32 txHash, address destination, uint value, bytes data)
-	internal
-	notNull(destination)
-	{
+	  notNull(destination)
+	  private {
 		sideChainTx[txHash] = Transaction({
 			destination: destination,
 			value: value,
@@ -180,9 +192,21 @@ contract SideChain is Freezable {
 		sideChainTxCount += 1;
 	}
 
+	/// @dev Allows an owner to confirm a transaction.
+	/// @param txHash to be confirmed
+	function confirmTransaction(bytes32 txHash)
+	  ownerExists(msg.sender)
+	  private {
+		require(!isSignedSC[txHash][msg.sender]);
+		require(sideChainTx[txHash].destination != 0);
+		isSignedSC[txHash][msg.sender] = true;
+		emit Confirmation(msg.sender, txHash);
+		executeTransaction(txHash);
+	}
+
 	// call has been separated into its own function in order to take advantage
 	// of the Solidity's code generator to produce a loop that copies tx.data into memory.
-	function external_call(address destination, uint value, uint dataLength, bytes data) private returns (bool) {
+	function external_call(address destination, uint value, uint dataLength, bytes data) private returns(bool) {
 		bool result;
 		assembly {
 			let x := mload(0x40)   // "Allocate" memory for output (0x40 is where "free memory" pointer is stored by convention)
@@ -206,10 +230,19 @@ contract SideChain is Freezable {
 	// SUBMIT SIGNATURES TO WITHDRAW ON MAINCHAIN
 	//////////////////////////////////////////////
 
-	function submitSignatureMC(bytes32 msgHash, bytes32 txHash, address destination, uint256 value, bytes data, uint8 v, bytes32 r, bytes32 s) public
-		ownerExists(msg.sender)
-		notNull(destination)
-	{
+	/// @dev store signature to be submitted in mainchain
+	/// @param msgHash sha3 hash of txHash destination value data and VERSION
+	/// @param txHash transaction hash of the deposit tx in side chain
+	/// @param destination destination provided in deposit tx in side chain
+	/// @param value msg.value of deposit tx in side chain
+	/// @param data data of deposit tx in side chain
+	/// @param v part of sig
+	/// @param r part of sig
+	/// @param s part of sig
+	function submitSignatureMC(bytes32 msgHash, bytes32 txHash, address destination, uint256 value, bytes data, uint8 v, bytes32 r, bytes32 s)
+	  ownerExists(msg.sender)
+	  notNull(destination)
+	  public {
 		require(!isSignedMC[txHash][msg.sender]);
 
 		address signer = ecrecover(msgHash, v, r, s);
@@ -228,15 +261,21 @@ contract SideChain is Freezable {
 		emit SignatureAdded(txHash, v, r, s);
 	}
 
-	function addSignatureMC(bytes32 txHash, uint8 v, bytes32 r, bytes32 s) internal {
+	/// @dev store the signature
+	function addSignatureMC(bytes32 txHash, uint8 v, bytes32 r, bytes32 s) private {
 		mainChainSigs[txHash].v.push(v);
 		mainChainSigs[txHash].r.push(r);
 		mainChainSigs[txHash].s.push(s);
 	}
 
+	/// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
+	/// @param txHash Transaction transactionHash
+	/// @param destination Transaction target address.
+	/// @param value Transaction ether value.
+	/// @param data Transaction data payload.
 	function addTransactionMC(bytes32 txHash, address destination, uint256 value, bytes data)
-		internal
-		notNull(destination)
+	  notNull(destination)
+	  private
 	{
 		mainChainTxs[txHash] = Transaction({
 			destination: destination,
@@ -247,7 +286,12 @@ contract SideChain is Freezable {
 		mainChainTxCount += 1;
 	}
 
-	function getTransactionMC(bytes32 txHash) view public returns (address destination, uint256 value, bytes data, uint8[] v, bytes32[] r, bytes32[] s) {
+	/// @dev retrieve the value and signatures of a txHash
+	/// @param txHash to get
+	function getTransactionMC(bytes32 txHash)
+	  view
+	  public
+	  returns(address destination, uint256 value, bytes data, uint8[] v, bytes32[] r, bytes32[] s) {
 		Transaction storage tempTx = mainChainTxs[txHash];
 		Signatures storage tempSigs = mainChainSigs[txHash];
 		return (tempTx.destination, tempTx.value, tempTx.data, tempSigs.v, tempSigs.r, tempSigs.s);
