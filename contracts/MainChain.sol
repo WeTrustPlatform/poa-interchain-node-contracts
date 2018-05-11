91fb3f9fda94773e7caaf1482637542c81948bd2
shine2lay <aungshine@gmail.com>
2018-05-10 18:32:36 -0700

    documentation + clean up mainchain.sol

--- contracts/MainChain.sol
+++ contracts/MainChain.sol
@@ -3,17 +3,15 @@ pragma solidity ^0.4.21;
 import './libs/MultiSigOwnable.sol';
 import './libs/Freezable.sol';
 
-/// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
-/// @author Stefan George - <stefan.george@consensys.net>
 contract MainChain is Freezable {
-	/*
-	 * Version information
-	 */
+	//////////////////////
+	//	 CONSTANTS
+	/////////////////////
 	uint8 public constant VERSION = 1;
 
-	/*
-	 *  Events
-	 */
+	//////////////////////
+	//	 EVENTS
+	/////////////////////
 	event Execution(bytes32 indexed txHash);
 	event ExecutionFailure(bytes32 indexed txHash);
 	event Deposit(address indexed sender, address indexed to, uint value);
@@ -21,34 +19,9 @@ contract MainChain is Freezable {
 	event UnBlackListed(bytes32 indexed txhash);
 	event EmergencyWithdrawal(bytes32 hashedParam, address indexed destination, uint256 value, bytes data);
 
-	/*
-	 *  Storage
-	 */
-	mapping (bytes32 => Transaction) public transactions;
-	mapping (bytes32 => EmergencyTx) public emergencyTxs;
-	mapping (address => mapping(address => bool)) public emergencyTxConfirmation;
-	mapping (bytes32 => bool) public isBlackListed;
-	uint256 public transactionCount;
-
-	struct Transaction {
-		address destination;
-		uint256 value;
-		bytes data;
-	}
-
-	struct EmergencyTx {
-		address destination;
-		uint256 value;
-		bytes data;
-		bool executed;
-		address[] confirmations;
-		mapping(address => bool) isConfirmed;
-	}
-
-	/*
-	 *  Modifiers
-	 */
-
+	//////////////////////
+	//	 Modifiers
+	/////////////////////
 	modifier transactionDoesNotExists(bytes32 txHash) {
 		require(transactions[txHash].destination == 0);
 		_;
@@ -79,35 +52,65 @@ contract MainChain is Freezable {
 		_;
 	}
 
-	/*
-	 * Public functions
-	 */
+	////////////////////////
+	//	 Storage Variables
+	////////////////////////
+	mapping (bytes32 => Transaction) public transactions;
+	mapping (bytes32 => EmergencyTx) public emergencyTxs;
+	mapping (address => mapping(address => bool)) public emergencyTxConfirmation;
+	mapping (bytes32 => bool) public isBlackListed;
+	uint256 public transactionCount;
+
+	struct Transaction {
+		address destination;
+		uint256 value;
+		bytes data;
+	}
+
+	struct EmergencyTx {
+		address destination;
+		uint256 value;
+		bytes data;
+		bool executed;
+		address[] confirmations;
+		mapping(address => bool) isConfirmed;
+	}
+
+
+
+	/////////////////////////
+	//	 Public Functions
+	////////////////////////
+
 	/// @dev Contract constructor sets initial owners and required number of confirmations.
 	/// @param _owners List of initial owners.
 	/// @param _required Number of required confirmations.
-	function MainChain(address[] _owners, uint8 _required) Freezable(_owners, _required)
-		public
-	{
-	}
+	function MainChain(address[] _owners, uint8 _required) Freezable(_owners, _required) public {}
 
-	function deposit(address to) notNull(to) onlyWhenNotFrozen payable public {
+	function deposit(address to)
+	  onlyWhenNotFrozen
+	  notNull(to)
+	  payable
+	  public {
 		require(msg.value > 0);
 		emit Deposit(msg.sender, to, msg.value);
 	}
 
-	/// @dev Allows an owner to submit and confirm a transaction.
-	/// @param destination Transaction target address.
-	/// @param value Transaction ether value.
-	/// @param data Transaction data payload.
-	/// @return Returns transaction ID.
+	/// @dev submit a transaction to be processed
+	/// @param msgHash sha3 hash of txHash destination value data and VERSION
+	/// @param txHash transaction hash of the deposit tx in side chain
+	/// @param destination destination provided in deposit tx in side chain
+	/// @param value msg.value of deposit tx in side chain
+	/// @param data data of deposit tx in side chain
+	/// @param v list of v part of sig
+	/// @param r list of r part of sig
+	/// @param s list of s part of sig
 	function submitTransaction(bytes32 msgHash, bytes32 txHash, address destination, uint256 value, bytes data, uint8[] v, bytes32[] r, bytes32[] s)
-		public
-		notNull(destination)
-		transactionDoesNotExists(txHash)
-		txNotBlackListed(txHash)
-		onlyWhenNotFrozen
-		returns (bytes32)
-	{
+	  notNull(destination)
+	  transactionDoesNotExists(txHash)
+	  txNotBlackListed(txHash)
+	  onlyWhenNotFrozen
+	  public {
 		require(v.length >= required);
 
 		bytes32 hashedTxParams = keccak256(txHash, destination, value, data, VERSION);
@@ -124,99 +127,39 @@ contract MainChain is Freezable {
 		}
 	}
 
-	function hasEnoughRequiredSignatures(bytes32 msgHash, uint8[] v, bytes32[] r, bytes32[] s) internal view returns(bool){
-		address[] memory confirmed = new address[](v.length);
-		uint8 confirmedCount;
-
-		for (uint8 i = 0; i < v.length; i++) {
-			address result = ecrecover(msgHash, v[i], r[i], s[i]);
-
-			if (!duplicateExists(confirmed, result)) {
-				confirmed[i] = result;
-				confirmedCount++;
-			}
-
-			if (confirmedCount >= required) return true;
-		}
-		return false;
-	}
-
-	function duplicateExists(address[] list, address toCheck) internal pure returns (bool) {
-		for (uint8 i = 0; i < list.length; i++) {
-			if (list[i] == toCheck) return true;
-		}
-		return false;
-	}
-
-	// call has been separated into its own function in order to take advantage
-	// of the Solidity's code generator to produce a loop that copies tx.data into memory.
-	function external_call(address destination, uint value, uint dataLength, bytes data) private returns (bool) {
-		bool result;
-		assembly {
-			let x := mload(0x40)   // "Allocate" memory for output (0x40 is where "free memory" pointer is stored by convention)
-			let d := add(data, 32) // First 32 bytes are the padded length of data, so exclude that
-			result := call(
-			sub(gas, 34710),   // 34710 is the value that solidity is currently emitting
-			// It includes callGas (700) + callVeryLow (3, to pay for SUB) + callValueTransferGas (9000) +
-			// callNewAccountGas (25000, in case the destination address does not exist and needs creating)
-			destination,
-			value,
-			d,
-			dataLength,        // Size of the input (in bytes) - this is what fixes the padding problem
-			x,
-			0                  // Output is ignored, therefore the output size is zero
-			)
-		}
-		return result;
-	}
-
-	/*
-	 * Internal functions
-	 */
-	/// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
-	/// @param destination Transaction target address.
-	/// @param value Transaction ether value.
-	/// @param data Transaction data payload.
-	/// @return Returns transaction ID.
-	function addTransaction(bytes32 txHash, address destination, uint value, bytes data)
-		internal
-		notNull(destination)
-	{
-		transactions[txHash] = Transaction({
-			destination: destination,
-			value: value,
-			data: data
-			});
-		transactionCount += 1;
-	}
-
-	// @TODO still needs to write test cases for addBlackList and removeBlackList
+	/// @dev Allow to add a txHash to blacklist which will prevent a submitTransaction with txHash to fail
+	/// @param txHash to be blackListed
 	function addBlackList(bytes32 txHash)
-		public
-		onlyByWallet
-		txNotBlackListed(txHash)
-		onlyWhenNotFrozen
-	{
+	  onlyByWallet
+	  onlyWhenNotFrozen
+	  txNotBlackListed(txHash)
+	  public {
 		isBlackListed[txHash] = true;
 
 		emit BlackListed(txHash);
 	}
 
+	/// @dev Remove a particular txHash from the blacklist
+	/// @param txHash to be removed from blackListed
 	function removeBlackList(bytes32 txHash)
-		public
-		onlyByWallet
-		txBlackListed(txHash)
-		onlyWhenNotFrozen
-	{
+	  onlyByWallet
+	  onlyWhenNotFrozen
+	  txBlackListed(txHash)
+	  public {
 		isBlackListed[txHash] = false;
 
 		emit UnBlackListed(txHash);
 	}
 
-
-	//@dev A method to get the money out when the contract is frozen
+	/// @dev Allows owner to move funds when frozen
+	/// @param destination target destination
+	/// @param value ether value
+	/// @param data data payload
 	// @TODO still need to write tests for emergency withdrawal
-	function emergencyWithdrawal(address destination, uint256 value, bytes data) ownerExists(msg.sender) onlyWhenFrozen public {
+	function emergencyWithdrawal(address destination, uint256 value, bytes data)
+	  onlyWhenFrozen
+	  ownerExists(msg.sender)
+	  public {
 		bytes32 hashedParam = keccak256(destination, value, data);
 
 		EmergencyTx storage transaction = emergencyTxs[hashedParam];
@@ -242,5 +185,79 @@ contract MainChain is Freezable {
 				emit ExecutionFailure(hashedParam);
 			}
 		}
+	}
+
+
+	/////////////////////////
+	//	 Private Functions
+	////////////////////////
+
+	/// @dev checks all the signatures and make sure they are from the owners
+	/// @param msgHash messageHash that signers singed
+	/// @param v , r, s signatures
+	/// @returns boolean
+	function hasEnoughRequiredSignatures(bytes32 msgHash, uint8[] v, bytes32[] r, bytes32[] s) view private returns(bool){
+		address[] memory confirmed = new address[](v.length);
+		uint8 confirmedCount;
+
+		for (uint8 i = 0; i < v.length; i++) {
+			address result = ecrecover(msgHash, v[i], r[i], s[i]);
+
+			if (!duplicateExists(confirmed, result)) {
+				confirmed[i] = result;
+				confirmedCount++;
+			}
+
+			if (confirmedCount >= required) return true;
+		}
+		return false;
+	}
+
+	/// @dev check that no duplicate exists
+	/// @param list array to be checked
+	/// @param toCheck element to check for
+	function duplicateExists(address[] list, address toCheck) pure private returns(bool) {
+		for (uint8 i = 0; i < list.length; i++) {
+			if (list[i] == toCheck) return true;
+		}
+		return false;
+	}
+
+	// call has been separated into its own function in order to take advantage
+	// of the Solidity's code generator to produce a loop that copies tx.data into memory.
+	function external_call(address destination, uint value, uint dataLength, bytes data) private returns(bool) {
+		bool result;
+		assembly {
+			let x := mload(0x40)   // "Allocate" memory for output (0x40 is where "free memory" pointer is stored by convention)
+			let d := add(data, 32) // First 32 bytes are the padded length of data, so exclude that
+			result := call(
+			sub(gas, 34710),   // 34710 is the value that solidity is currently emitting
+			// It includes callGas (700) + callVeryLow (3, to pay for SUB) + callValueTransferGas (9000) +
+			// callNewAccountGas (25000, in case the destination address does not exist and needs creating)
+			destination,
+			value,
+			d,
+			dataLength,        // Size of the input (in bytes) - this is what fixes the padding problem
+			x,
+			0                  // Output is ignored, therefore the output size is zero
+			)
+		}
+		return result;
+	}
+
+	/// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
+	/// @param txHash Transaction transactionHash
+	/// @param destination Transaction target address.
+	/// @param value Transaction ether value.
+	/// @param data Transaction data payload.
+	function addTransaction(bytes32 txHash, address destination, uint value, bytes data)
+	  notNull(destination)
+	  private {
+		transactions[txHash] = Transaction({
+			destination: destination,
+			value: value,
+			data: data
+			});
+		transactionCount += 1;
 	}
 }
