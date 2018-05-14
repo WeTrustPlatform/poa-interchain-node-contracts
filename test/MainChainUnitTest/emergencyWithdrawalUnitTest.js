0c4f5b0e60e3a5ebb8363bead7356632d0436893
Yuan Tu <yuantu@wetrust.io>
2018-05-14 13:36:06 -0700

    address comments

--- test/MainChainUnitTest/emergencyWithdrawalUnitTest.js
+++ test/MainChainUnitTest/emergencyWithdrawalUnitTest.js
@@ -25,12 +25,20 @@ let value;
 let data;
 let sigs;
 let version;
+let isFrozen;
 
 contract('MainChain: emergencyWithdrawal Unit Test', function(accounts) {
   beforeEach(async function () {
     mainchainInstance = await mainchain.new(accounts.slice(0, 3), 2);
     destinationMainchainInstance = await mainchain.new(accounts.slice(0, 3), 2);
     await mainchainInstance.deposit(accounts[0], {from: accounts[0], value: 1e7});
+
+    // freeze mainchainInstance.
+    isFrozen = await mainchainInstance.checkIfFrozen.call();
+    assert.notOk(isFrozen);
+    await mainchainInstance.freeze();
+    isFrozen = await mainchainInstance.checkIfFrozen.call();
+    assert.ok(isFrozen);
 
     exampleTokenInstance = await exampleToken.new(accounts.slice(0, 4));
 
@@ -45,15 +53,8 @@ contract('MainChain: emergencyWithdrawal Unit Test', function(accounts) {
     const contractBalanceBefore = web3.eth.getBalance(mainchainInstance.address);
     const desContractBalanceBefore = web3.eth.getBalance(toAddress);
 
-    // check mainchainInstance is fronzen.
-    let isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.notOk(isFrozen);
-    await mainchainInstance.freeze();
-    isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.ok(isFrozen);
-
     // need approvals from 2 owners.
-    const transferEncodedData = mainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
+    const transferEncodedData = destinationMainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
     await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[0]});
     const res = await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[1]});
 
@@ -69,67 +70,79 @@ contract('MainChain: emergencyWithdrawal Unit Test', function(accounts) {
   });
 
   it("checks that ERC20 Token emergency withdrawal works as intended if all the condition are valid", async function () {
+
+    const tokenAmountToSend = 1000;
+
     // send 1e7 exampleToken to the mainchainInstance first
     await exampleTokenInstance.transfer(mainchainInstance.address, value, {from: accounts[0]});
-    const contractBalanceBefore = await exampleTokenInstance.balanceOf.call(mainchainInstance.address);
-    const desContractBalanceBefore = await exampleTokenInstance.balanceOf.call(toAddress);
-
-    // check mainchainInstance is fronzen.
-    let isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.notOk(isFrozen);
-    await mainchainInstance.freeze();
-    isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.ok(isFrozen);
+    const tokenBalanceBefore = await exampleTokenInstance.balanceOf.call(mainchainInstance.address);
+    const destTokenBalanceBefore = await exampleTokenInstance.balanceOf.call(toAddress);
 
     // need approvals from 2 owners.
-    const tokenAmount = 1000;
-    const transferEncodedData = exampleTokenInstance.contract.transfer.getData(toAddress, tokenAmount);
+    const transferEncodedData = exampleTokenInstance.contract.transfer.getData(toAddress, tokenAmountToSend);
 
-    await mainchainInstance.emergencyWithdrawal(toAddress, 0, transferEncodedData, {from: accounts[0]});
-    const res = await mainchainInstance.emergencyWithdrawal(toAddress, 0, transferEncodedData, {from: accounts[1]});
+    value = 0;
+    await mainchainInstance.emergencyWithdrawal(exampleTokenInstance.address, value, transferEncodedData, {from: accounts[0]});
+    const res = await mainchainInstance.emergencyWithdrawal(exampleTokenInstance.address, value, transferEncodedData, {from: accounts[1]});
+
+    // check EmergencyWithdrawal event is fired.
+    assert.equal(res.logs[0].event, 'EmergencyWithdrawal');
 
     // check funds has been moved from mainchainInstance to destinationMainchainInstance.
-    const contractBalanceAfter = await exampleTokenInstance.balanceOf.call(mainchainInstance.address);
-    const desContractBalanceAfter = await exampleTokenInstance.balanceOf.call(toAddress);
+    const tokenBalanceAfter = await exampleTokenInstance.balanceOf.call(mainchainInstance.address);
+    const destTokenBalanceAfter = await exampleTokenInstance.balanceOf.call(toAddress);
 
-    assert.equal(contractBalanceBefore - contractBalanceAfter, tokenAmount);
-    assert.equal(desContractBalanceAfter - desContractBalanceBefore, tokenAmount);
+    assert.equal(tokenBalanceBefore - tokenBalanceAfter, tokenAmountToSend);
+    assert.equal(destTokenBalanceAfter - destTokenBalanceBefore, tokenAmountToSend);
   });
 
 
   it("revert if contract is not frozen", async function () {
     // check mainchainInstance is not frozen.
-    let isFrozen = await mainchainInstance.checkIfFrozen.call();
+    isFrozen = await mainchainInstance.checkIfFrozen.call();
+    assert.ok(isFrozen);
+    await mainchainInstance.unFreeze();
+    isFrozen = await mainchainInstance.checkIfFrozen.call();
     assert.notOk(isFrozen);
 
-    const transferEncodedData = mainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
+    const transferEncodedData = destinationMainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
     await utils.assertRevert(mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[0]}));
+
+    // check when mainchainInstance is frozen, emergencyWithdrawal works as intended.
+    await mainchainInstance.freeze();
+    isFrozen = await mainchainInstance.checkIfFrozen.call();
+    assert.ok(isFrozen);
+
+    await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[0]});
+    const res = await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[1]});
+
+    // check EmergencyWithdrawal event is fired.
+    assert.equal(res.logs[1].event, 'EmergencyWithdrawal');
   });
 
   it("revert if msg.sender is not owner", async function () {
-    // check mainchainInstance is fronzen.
-    let isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.notOk(isFrozen);
-    await mainchainInstance.freeze();
-    isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.ok(isFrozen);
-
     // one less signature than required
-    const transferEncodedData = mainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
+    const transferEncodedData = destinationMainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
     await utils.assertRevert(mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[5]}));
+
+    // check when msg.sender is owner, emergencyWithdrawal works as intended.
+    await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[0]});
+    const res = await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[1]});
+
+    // check EmergencyWithdrawal event is fired.
+    assert.equal(res.logs[1].event, 'EmergencyWithdrawal');
   });
 
   it("revert if there aren't enough valid signatures", async function () {
-    // check mainchainInstance is fronzen.
-    let isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.notOk(isFrozen);
-    await mainchainInstance.freeze();
-    isFrozen = await mainchainInstance.checkIfFrozen.call();
-    assert.ok(isFrozen);
-
     // two of the same signature
-    const transferEncodedData = mainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
+    const transferEncodedData = destinationMainchainInstance.contract.deposit.getData(toAddress, {from: mainchainInstance.address, value});
     await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[0]});
     await utils.assertRevert(mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[0]}));
+
+    // check when there are enough valid signatures, emergencyWithdrawal works as intended.
+    const res = await mainchainInstance.emergencyWithdrawal(toAddress, value, transferEncodedData, {from: accounts[1]});
+
+    // check EmergencyWithdrawal event is fired.
+    assert.equal(res.logs[1].event, 'EmergencyWithdrawal');
   });
 });
